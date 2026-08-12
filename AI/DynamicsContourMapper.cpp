8e958fc8f1b52c0a038bfd07b36c8974991e5c65
ThreePeak <threepeak@users.noreply.github.com>
2026-08-12 01:00:42 -0700

    feat(ai): Register xLights/AI/DynamicsContourMapper.cpp in CMakeLists.txt target

---
+++ AI/DynamicsContourMapper.cpp
@@ -0,0 +1,15 @@
+/***************************************************************
+ * This source file comes from the xLights project
+ * https://www.xlights.org
+ * https://github.com/xLightsSequencer/xLights
+ * Copyright claimed based on commit dates recorded in Github
+ * License: https://github.com/xLightsSequencer/xLights/blob/master/License.txt
+ **************************************************************/
+
+#include "DynamicsContourMapper.h"
+
+namespace xLights::AI {
+
+// DynamicsContourMapper implementation is forwarded to AudioDynamicsMapper.
+
+} // namespace xLights::AI
---
+++ AI/DynamicsContourMapper.h
@@ -0,0 +1,19 @@
+#pragma once
+
+/***************************************************************
+ * This source file comes from the xLights project
+ * https://www.xlights.org
+ * https://github.com/xLightsSequencer/xLights
+ * Copyright claimed based on commit dates recorded in Github
+ * License: https://github.com/xLightsSequencer/xLights/blob/master/License.txt
+ **************************************************************/
+
+#include "media/AudioDynamicsMapper.h"
+
+namespace xLights::AI {
+
+using DynamicsContourMapper = AudioDynamicsMapper;
+using AudioFrameContour = ::AudioFrameContour;
+using AudioDynamicsContourResult = ::AudioDynamicsContourResult;
+
+} // namespace xLights::AI
